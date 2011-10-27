bd3ac708c25e6ab5ade7c251c64f20d96a1a64be
Jim Allman <jim@ibang.com>
2011-10-27 14:09:12 -0400

    Smarter internal queries use Django ORM, except for callable service method get_sessions_using_resource()

--- pr_services/resource_system/resource_manager.py
+++ pr_services/resource_system/resource_manager.py
@@ -51,81 +51,52 @@ class ResourceManager(ObjectManager):
         return r
 
     @service_method
-    # TODO: confirm that service_method is appropriate here
     def find_available_resources(self, auth_token, start, end):
-        session_manager = facade.managers.SessionManager()
-        res_requirement_manager = facade.managers.SessionResourceTypeRequirementManager()
-        # start with all IDs, then filter out those who are associated with sessions during this time
-        available_resource_info = self.get_filtered(auth_token, {})
-        available_resource_ids = [res['id'] for res in available_resource_info]
-        # eg,  [{'id': 1}, {'id': 2}, {'id': 3}, {'id': 4}]
-        
+        # build a list of blocked resources, then exclude them from final results
+        blocked_resource_ids = []
+
         # convert time arguments from isoformat string to datetime, if not already
-        #if isinstance(start, basestring):
-        #    start = pr_time.iso8601_to_datetime(start)
-        #if isinstance(end, basestring):
-        #    end = pr_time.iso8601_to_datetime(end)
-            
-        conflicting_sessions = session_manager.get_filtered(auth_token,
-            #{ 'greater_than_or_equal' : {'start' : start },
-            #  'less_than_or_equal' : {'end' : end } }, 
-              {'or' : [  # if session start OR end-time falls within the test period...
-                {'range' : {'start': [start, end],},},
-                {'range' : {'end': [start, end],},},
-                ],
-              },       
-            ['name', 'status', 'session_resource_type_requirements'])
+        if isinstance(start, basestring):
+            start = pr_time.iso8601_to_datetime(start)
+        if isinstance(end, basestring):
+            end = pr_time.iso8601_to_datetime(end)
         
+        # emulate logical OR by combining query-sets
+        conflicting_sessions = (
+            facade.models.Session.objects.filter(start__range=[start, end]) | 
+            facade.models.Session.objects.filter(end__range=[start, end])
+        )
         for sess in conflicting_sessions:
-            for req_id in sess['session_resource_type_requirements']:
-                blocked_resource_info = res_requirement_manager.get_filtered(auth_token,
-                     {'member' : {'resources' : [req_id]} }, [])
-                blocked_resource_ids = [res['id'] for res in blocked_resource_info]
+            for req in sess.session_resource_type_requirements.all():
+                # add its resource IDs to our growing list
+                blocked_resource_ids.extend( req.resources.all().values_list('id', flat=True) )
             
-                # chase resource-type requirements to any underlying resources
-                for blocked_id in blocked_resource_ids:
-                    if blocked_id in available_resource_ids:
-                        available_resource_ids.remove( blocked_id )
-                
-        return self.get_filtered(auth_token, {'member' : {'id' : available_resource_ids} }, ['name', 'description'])
+        # remove duplicates from the list of blocked IDs
+        blocked_resource_ids = list(set(blocked_resource_ids))
+        # return all NON-blocked resources (ie, those still available during this time)
+        return self.get_filtered(auth_token, {'not': {'member' : {'id' : blocked_resource_ids} } }, ['name', 'description'])
 
     @service_method    
     def resource_used_during(self, auth_token, resource_id, start, end):
         # A more targeted "probe" for a particular Resource during a specified time span.
         # Returns True if the resource is already scheduled within the chosen duration, False if not
         res_requirement_manager = facade.managers.SessionResourceTypeRequirementManager()
-        session_manager = facade.managers.SessionManager()
         related_sessions = res_requirement_manager.get_sessions_using_resource(auth_token, resource_id)
+        related_session_ids = [s['id'] for s in related_sessions ]
 
-        #if isinstance(start, basestring):
-        #    start = pr_time.iso8601_to_datetime(start)
-        #if isinstance(end, basestring):
-        #    end = pr_time.iso8601_to_datetime(end)
+        if isinstance(start, basestring):
+            start = pr_time.iso8601_to_datetime(start)
+        if isinstance(end, basestring):
+            end = pr_time.iso8601_to_datetime(end)
     
-        session_ids = [s['id'] for s in related_sessions ]  # use list comprehension to extract just IDs?
-        conflicting_sessions = session_manager.get_filtered(auth_token, 
-            {'and': [
-             {'member' : {'id': session_ids},},
-             {'or' : [  # if session start OR end-time falls within the test period...
-                {'range' : {'start': [start, end],},},
-                {'range' : {'end': [start, end],},},
-                # TODO: This collides on boundary values (eg, exact start time)! 
-                # Can we opt to disregard boundary values?
-                
-                # TODO: why didn't this work? wrong types!?
-                #{'and' : [
-                #        {'greater_than' : {'start' : start}},
-                #        {'less_than' : {'end' : start}},
-                #         ]
-                #},
-                #{'and' : [
-                #        {'greater_than' : {'start' : end}},
-                #        {'less_than' : {'end' : end}},
-                #         ],
-                #}
-                ],
-              },
-             ],}, ['start','end'])
+        # This collides on boundary values (eg, identical test-end and session-start times)
+        # TODO: Should we opt to disregard boundary values?
+        related_sessions = facade.models.Session.objects.filter(pk__in=related_session_ids)
+        conflicting_sessions = (
+            related_sessions.filter(start__range=[start, end]) | 
+            related_sessions.filter(end__range=[start, end])
+        )
+
         if len(conflicting_sessions) > 0:
             return True
         return False # no conflict found
