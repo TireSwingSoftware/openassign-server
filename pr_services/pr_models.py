912a04887c474886f4cc55e2b4c7144fd75a3bc1
John O'Connor <tehjcon@gmail.com>
2012-01-06 18:26:10 -0500

    Sort imports in pr_models.py

--- pr_services/pr_models.py
+++ pr_services/pr_models.py
@@ -1,26 +1,32 @@
-from datetime import date, datetime, time, timedelta
-from decimal import Decimal
+
 import cPickle
 import logging
 import random
 import re
 import shutil
-from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
-from django.db import models
-from django.template import Template, Context
-from django.utils.hashcompat import sha_constructor
+
+from datetime import date, datetime, time, timedelta
+from decimal import Decimal
+
 import django.core.exceptions # for ValidationError
 import django.db
 import django.db.models.fields.related
 import django.forms.util # for ValidationError
+
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.core.cache import cache
+from django.db import models
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
-from django.core.cache import cache
+from django.template import Template, Context
+from django.utils.hashcompat import sha_constructor
+
 import exceptions
 import facade
-import storage
 import memcache
+import storage
+
 from fields import *
 from pr_services.middleware import get_client_ip
 
