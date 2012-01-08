8c24447fdb34596c1e6bb801be2edc654f969c1d
John O'Connor <tehjcon@gmail.com>
2012-01-08 04:07:04 -0500

    Django tests will now import only test classes from inner test suites.

--- pr_services/tests.py
+++ pr_services/tests.py
@@ -1,2 +1,40 @@
-from pr_services.pr_tests import *
-from pr_services.exam_system.tests import *
+"""
+Merge test classes from multiple test suites into this namespace so that
+they can be easily started by a typical Django test run.
+
+
+This module also ensures when test classes are merged into a single
+namepsace, there are no duplicates names.
+"""
+
+import inspect
+
+from pr_services.testlib import TestCase
+
+TEST_MODULES = (
+        'pr_services.pr_tests',
+        'pr_services.exam_system.tests',
+        # more coming soon...
+)
+
+def _import_test_module(path):
+    module = __import__(path, globals(), locals(), [], -1)
+    suites = path.split('.')[1:]
+    for suite in suites:
+        module = getattr(module, suite)
+    return module
+
+def _test_class_filter(name__value):
+    name, value = name__value
+    return name.startswith('Test') and isinstance(value, type)
+
+def _setup_tests():
+    for module_path in TEST_MODULES:
+        module = _import_test_module(module_path)
+        members = inspect.getmembers(module)
+        classobjs = dict(filter(_test_class_filter, members))
+        assert not any(name in locals() for name in classobjs)
+        globals().update(classobjs)
+        locals().update(classobjs)
+
+_setup_tests()
