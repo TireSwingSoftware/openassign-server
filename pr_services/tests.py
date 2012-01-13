6892dba87bb2bc2f70a7c8091378d6a8c549032b
John O'Connor <tehjcon@gmail.com>
2012-01-13 16:40:06 -0500

    Tune up main test class importer
    - Also fixes some problems with properly setting locals in
      the previous draft

--- pr_services/tests.py
+++ pr_services/tests.py
@@ -17,8 +17,8 @@ TEST_MODULES = (
         # more coming soon...
 )
 
-def _import_test_module(path):
-    module = __import__(path, globals(), locals(), [], -1)
+def _import_test_module(path, xlocals, xglobals):
+    module = __import__(path, xglobals, xlocals, [], -1)
     suites = path.split('.')[1:]
     for suite in suites:
         module = getattr(module, suite)
@@ -26,15 +26,22 @@ def _import_test_module(path):
 
 def _test_class_filter(name__value):
     name, value = name__value
-    return name.startswith('Test') and isinstance(value, type)
+    if not isinstance(value, type):
+        return False
+    if not issubclass(value, TestCase) or value is TestCase:
+        return False
+    else:
+        return name.startswith('Test')
 
-def _setup_tests():
+def _setup_tests(xlocals, xglobals):
     for module_path in TEST_MODULES:
-        module = _import_test_module(module_path)
+        module = _import_test_module(module_path, xlocals, xglobals)
         members = inspect.getmembers(module)
-        classobjs = dict(filter(_test_class_filter, members))
-        assert not any(name in locals() for name in classobjs)
-        globals().update(classobjs)
-        locals().update(classobjs)
+        classes = dict(filter(_test_class_filter, members))
+        assert not any(name in xlocals or name in xglobals for name in classes)
+        if xglobals:
+            xglobals.update(classes)
+        if xlocals:
+            xlocals.update(classes)
 
-_setup_tests()
+_setup_tests(locals(), globals())
