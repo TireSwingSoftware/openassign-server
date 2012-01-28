3a34d0355924c6d909c4b50e6ddbe7a69a6ff39c
John O'Connor <tehjcon@gmail.com>
2012-01-27 20:24:33 -0500

    Add load_fixtures test decorator

--- pr_services/testlib/helpers.py
+++ pr_services/testlib/helpers.py
@@ -4,6 +4,8 @@ Convenient routines for facilitating common test operations.
 
 import functools
 
+from django.core.management import call_command
+
 from pr_services.exceptions import PermissionDeniedException
 
 
@@ -28,3 +30,31 @@ def expectPermissionDenied(func):
             func.__doc__ or func.__name__)
 
     return wrapper
+
+
+def load_fixtures(*fixtures):
+    """
+    Decorator for loading fixtures for a single test method.
+
+    Example:
+        class FooTest(TestCase):
+            # fixtures for all test methods
+            fixtures = ['some_big_fixture']
+
+            def test_foo(self):
+                ...
+
+            @load_fixtures('small_fixture1', 'small_fixture2')
+            def test_bar(self):
+                # do something testy
+                ...
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            call_command('loaddata', *fixtures,
+                    **{'verbosity': 0})
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
