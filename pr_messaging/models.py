04fcc037f51a0ba079dd85b1b02d0b7c66fc3cec
Michael Hrivnak <mhrivnak@hrivnak.org>
2012-02-16 01:38:30 -0500

    re #99 discovered that when using the Flex UI, more newline characters can be introduced into the subject template, so we now remove all of them from the rendered subject. Newlines are not allowed in email headers.

--- pr_messaging/models.py
+++ pr_messaging/models.py
@@ -79,9 +79,11 @@ class MessageTemplate(models.Model):
 
     def render(self, context):
         subject = Template(self.subject).render(context)
-        # it is easy with templates to end up with a trailing \n. We must strip it here.
-        if subject.endswith('\n'):
-            subject = subject[:-1]
+        # it is easy with templates to end up with one or more unintended
+        # newline character. We must remove them here, because they aren't
+        # allowed in headers.
+        subject = subject.replace('\n', '')
+        subject = subject.replace('\r', '')
         body = Template(self.body).render(context)
         # Tries to render a plain text format as well for HTML emails.
         # FIXME: Should really find a more generic way to add attachments and
